6da900eff1103c9ed440868f3df688ef6f660d98
Plume <a.pristchepa@gmail.com>
2023-03-17 17:37:19 +0100

    feat: new color when selecting stuff

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,16 +1,18 @@
 require('onedark').setup {
-    style = "warm",
-    transparent = true,
-    lualine = {
-        transparent = true
-    },
-    barbar = {
-        transparent = true
-    }
+  style = "warm",
+  transparent = true,
+  lualine = {
+    transparent = true
+  },
+  barbar = {
+    transparent = true
+  }
 }
 vim.cmd.colorscheme "onedark"
 
 vim.cmd [[
     hi LineNr guibg=none guifg=#ffffff
     hi CursorLineNr guibg=none guifg=#ffffff
+    hi Visual guifg=#000000 guibg=#CBC3E3 gui=none
 ]]
+
