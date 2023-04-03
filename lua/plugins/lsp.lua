1055ebbbd5cd4d83ec66352401837b01b3bf2894
Plume羽 <38199330+Plumie@users.noreply.github.com>
2023-04-03 04:13:18 +0200

    feat: added virtual text

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,38 +3,39 @@ require("mason").setup()
 require('mason-lspconfig').setup({
   ensure_installed = {
     'tsserver',
-    'eslint',
     'html',
     'cssls'
   }
 })
 
 local lsp = require('lsp-zero').preset({
-
-  name = 'minimal',
   set_lsp_keymaps = true,
   manage_nvim_cmp = true,
   suggest_lsp_servers = false,
 })
 
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 lsp.setup()
+
 vim.diagnostic.config({
-  virtual_text = false, -- Turn off inline diagnostics
+  virtual_text = {
+    prefix = '●', -- Could be '■', '▎', 'x'
+  },
+  severity_sort = true,
 })
 
-
 vim.api.nvim_set_keymap(
-  'n', '<Leader>do', ':lua vim.diagnostic.open_float()<CR>', 
+  'n', '<Leader>dl', ':lua vim.diagnostic.goto_next()<CR>',
   { noremap = true, silent = true }
 )
 
 vim.api.nvim_set_keymap(
-  'n', '<Leader>dn', ':lua vim.diagnostic.goto_next()<CR>',
-  { noremap = true, silent = true }
-)
-
-vim.api.nvim_set_keymap(
-  'n', '<Leader>dp', ':lua vim.diagnostic.goto_prev()<CR>',
+  'n', '<Leader>dh', ':lua vim.diagnostic.goto_prev()<CR>',
   { noremap = true, silent = true }
 )
 
