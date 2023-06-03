73a98bb4171a3f9be9422bcbf4ce0f8752581f52
Plumie <a.pristchepa@gmail.com>
2023-06-03 11:23:05 +0200

    feat: oil settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,15 @@
 return {
   'stevearc/oil.nvim',
   config = function()
-    require('oil').setup()
+    require('oil').setup({
+       buf_options = {
+        buflisted = false
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+
+    vim.keymap.set('n', '<Leader>eo', ':Oil<CR>', {})
   end
 }
