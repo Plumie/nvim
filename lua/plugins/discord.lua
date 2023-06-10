3ddbfa7aea37153d32a7f5306c34249ae72fff68
Plumie <a.pristchepa@gmail.com>
2023-06-10 19:35:14 +0200

    fix: hmmm

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -2,5 +2,8 @@ return {
   'andweeb/presence.nvim',
   config = function()
     require('presence').setup()
+    vim.cmd [[
+      let g:presence_editing_text        = "Editing stuff"
+    ]]
   end
 }
