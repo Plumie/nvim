fad673b80930baba8d38ae35ccf268023e2f6d71
Plumie <a.pristchepa@gmail.com>
2023-12-24 18:38:22 +0100

    fix: indent

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,13 @@
 return {
   'zbirenbaum/copilot.lua',
   config = function()
-    require('copilot').setup({ suggestion = { auto_trigger = true, keymap = { accept = '<leader><Tab>' } } })
+    require('copilot').setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = '<leader><Tab>',
+        },
+      },
+    })
   end
 }
