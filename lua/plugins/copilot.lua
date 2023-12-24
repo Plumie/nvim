c03f5d5265b62a6a8c78744abc0a56eaec60ae9b
Plumie <a.pristchepa@gmail.com>
2023-12-24 21:10:56 +0100

    feat: idk

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,19 +1,19 @@
-    return {
-      'zbirenbaum/copilot.lua',
-      dependencies = {
-        'zbirenbaum/copilot-cmp',
-      },
-      config = function()
-        require('copilot').setup({
-          suggestion = {enabled = false},
-          panel = {enabled = false}
-          -- suggestion = {
-          --   auto_trigger = true,
-          --   keymap = {
-          --     accept = '<leader><Tab>',
-          --   },
-          -- },
-        })
-        require('copilot_cmp').setup()
-      end
-    }
+return {
+  'zbirenbaum/copilot.lua',
+  dependencies = {
+    'zbirenbaum/copilot-cmp',
+  },
+  config = function()
+    require('copilot').setup({
+      suggestion = {enabled = false},
+      panel = {enabled = false}
+      -- suggestion = {
+      --   auto_trigger = true,
+      --   keymap = {
+      --     accept = '<leader><Tab>',
+      --   },
+      -- },
+    })
+    require('copilot_cmp').setup()
+  end
+}
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -11,20 +11,16 @@ return {
     })
 
     vim.cmd.colorscheme "tokyonight-storm"
-
     -- Default
-
     vim.cmd [[
 			hi LineNr guibg=none guifg=#E1E3E4
       hi cursorlinenr guibg=none guifg=#BA9CF3
-      hi cursorline guibg=#2A2F38
+      hi cursorline guibg=#31304D
       hi visual guifg=#000000 guibg=#E4E3E1 gui=none
       hi statusline guibg=none
       hi WinSeparator guifg=white
 		]]
-
     -- Pounce
-
     vim.cmd [[
       hi pouncematch cterm=bold guifg=#E1E3E4 guibg=none gui=bold
       hi pouncegap cterm=bold guifg=#E1E3E4 guibg=none gui=bold
@@ -32,25 +28,19 @@ return {
       hi pounceacceptbest cterm=bold guifg=black guibg=#85D3F2 gui=bold
       hi pouncecursor cterm = bold guifg=black guibg=#cbc3e3 gui=bold
     ]]
-
     -- QuickScope
-
     vim.cmd [[
       hi QuickScopePrimary cterm=bold guifg=black guibg=#85D3F2 gui=bold
 			hi QuickScopeSecondary cterm=bold guifg=black guibg=#A5E179 gui=bold
     ]]
-
     -- CMP
-    
     vim.cmd [[
       hi PmenuSel guibg=#BA9CF3 guifg=#000000
       hi CmpItemAbbrMatch guifg=#BA9CF3
       hi CmpItemAbbrMatchFuzzy guifg=#BA9CF3
       hi CmpItemAbbrMatchFuzzyHighlight guifg=#BA9CF3
     ]]
-  
     -- Telescope
-
     vim.cmd [[
       hi TelescopeMatching guifg=#BA9CF3
       hi TelescopeBorder guifg=#BA9CF3
