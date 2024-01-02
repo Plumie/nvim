edc5f854308c78eb8b48bbd946e4dd403ce64980
Plumie <a.pristchepa@gmail.com>
2024-01-02 03:03:23 +0100

    feat: optimize map

--- lua/map.lua
+++ lua/map.lua
@@ -1,44 +1,45 @@
 local default = { noremap = true, silent = true }
+local keymap = vim.keymap
 
 -- Set space as leader key
-vim.keymap.set("n", "<Space>", "<Nop>", default)
+keymap.set("n", "<Space>", "<Nop>", default)
 vim.g.mapleader = " "
 
-vim.keymap.set('n', 'x', '"_x', default)
+keymap.set('n', 'x', '"_x', default)
 
 -- Prevent bad practices
-vim.keymap.set('i', '<Left>', '<Nop>', default)
-vim.keymap.set('i', '<Right>', '<Nop>', default)
-vim.keymap.set('i', '<Up>', '<Nop>', default)
-vim.keymap.set('i', '<Down>', '<Nop>', default)
+keymap.set('i', '<Left>', '<Nop>', default)
+keymap.set('i', '<Right>', '<Nop>', default)
+keymap.set('i', '<Up>', '<Nop>', default)
+keymap.set('i', '<Down>', '<Nop>', default)
 
-vim.keymap.set('n', '<Left>', '<Nop>', default)
-vim.keymap.set('n', '<Right>', '<Nop>', default)
-vim.keymap.set('n', '<Up>', '<Nop>', default)
-vim.keymap.set('n', '<Down>', '<Nop>', default)
+keymap.set('n', '<Left>', '<Nop>', default)
+keymap.set('n', '<Right>', '<Nop>', default)
+keymap.set('n', '<Up>', '<Nop>', default)
+keymap.set('n', '<Down>', '<Nop>', default)
 
-vim.keymap.set('v', '<Left>', '<Nop>', default)
-vim.keymap.set('v', '<Up>', '<Nop>', default)
-vim.keymap.set('v', '<Right>', '<Nop>', default)
-vim.keymap.set('v', '<Down>', '<Nop>', default)
+keymap.set('v', '<Left>', '<Nop>', default)
+keymap.set('v', '<Up>', '<Nop>', default)
+keymap.set('v', '<Right>', '<Nop>', default)
+keymap.set('v', '<Down>', '<Nop>', default)
 
 -- Buffer
-vim.keymap.set('n', '<leader>q', ':bdelete!<CR>', default)
-vim.keymap.set('n', '<leader>w', ':w!<CR>', default)
+keymap.set('n', '<leader>q', ':bdelete!<CR>', default)
+keymap.set('n', '<leader>w', ':w!<CR>', default)
 
 -- Navigate and center
-vim.keymap.set('n', '<C-d>', '<C-d>zz', default)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', default)
+keymap.set('n', '<C-d>', '<C-d>zz', default)
+keymap.set('n', '<C-u>', '<C-u>zz', default)
 
 -- Split
-vim.keymap.set('n', 'vs', ':vs<CR><C-w>l', default)
-vim.keymap.set('n', 'vd', ':split<CR><C-w>j', default)
+keymap.set('n', 'vs', ':vs<CR><C-w>l', default)
+keymap.set('n', 'vd', ':split<CR><C-w>j', default)
 
 -- Indentation
-vim.keymap.set('v', '<Tab>', '>><Esc>gv', default)
-vim.keymap.set('v', '<S-Tab>', '<<<Esc>gv', default)
+keymap.set('v', '<Tab>', '>><Esc>gv', default)
+keymap.set('v', '<S-Tab>', '<<<Esc>gv', default)
 
 -- Get old pasted text
-vim.keymap.set('n', 'gp', '`[v`]', default)
+keymap.set('n', 'gp', '`[v`]', default)
 
-vim.keymap.set('n', '<Esc>', '`[v`]', default)
+keymap.set('n', '<Esc>', '`[v`]', default)
