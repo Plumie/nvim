97c626018db61a8a82d878cc9a10f8f1bd8cdd2e
Plumie <a.pristchepa@gmail.com>
2024-01-04 11:51:37 +0100

    feat: improved structure

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
--- Setup Lazy
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Init config
-require('options')
-require('remap')
-require("lazy").setup('plugins')
+require('config.options')
+require('config.keymaps')
+require('config.lazy')
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,43 @@
+local default = { noremap = true, silent = true }
+local keymap = vim.keymap
+
+-- Remap leader to space
+keymap.set("n", "<Space>", "<Nop>", default)
+vim.g.mapleader = " "
+
+-- Prevent X from yanking to clipboard
+keymap.set('n', 'x', '"_x', default)
+
+-- Buffer management
+keymap.set('n', '<leader>q', ':bdelete!<CR>', default)
+keymap.set('n', '<leader>w', ':w!<CR>', default)
+
+-- Window management
+keymap.set('n', 'vs', ':vs<CR><C-w>l', default)
+keymap.set('n', 'vd', ':split<CR><C-w>j', default)
+
+-- Scroll and center
+keymap.set('n', '<C-d>', '<C-d>zz', default)
+keymap.set('n', '<C-u>', '<C-u>zz', default)
+
+-- Indentation
+keymap.set('v', '<Tab>', '>><Esc>gv', default)
+keymap.set('v', '<S-Tab>', '<<<Esc>gv', default)
+
+-- Get old pasted text
+keymap.set('n', 'gp', '`[v`]', default)
+keymap.set('n', '<Esc>', '`[v`]', default)
+
+-- Discipline
+keymap.set('i', '<Left>', '<Nop>', default)
+keymap.set('i', '<Right>', '<Nop>', default)
+keymap.set('i', '<Up>', '<Nop>', default)
+keymap.set('i', '<Down>', '<Nop>', default)
+keymap.set('n', '<Left>', '<Nop>', default)
+keymap.set('n', '<Right>', '<Nop>', default)
+keymap.set('n', '<Up>', '<Nop>', default)
+keymap.set('n', '<Down>', '<Nop>', default)
+keymap.set('v', '<Left>', '<Nop>', default)
+keymap.set('v', '<Up>', '<Nop>', default)
+keymap.set('v', '<Right>', '<Nop>', default)
+keymap.set('v', '<Down>', '<Nop>', default)
