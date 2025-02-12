ad5055e67748c1604b888ecfef357fa5f9a74eef
Plumie <a.pristchepa@gmail.com>
2025-02-12 15:26:28 +0100

    feat: changed pounce color

--- lua/plugins/pounce.lua
+++ lua/plugins/pounce.lua
@@ -14,8 +14,8 @@ return {
     vim.cmd [[
       hi pouncematch cterm=bold guifg=black guibg=#E1E3E4 gui=bold
       hi pouncegap cterm=bold guifg=black guibg=#cbc3e3 gui=bold
-      hi pounceaccept cterm=bold guifg=black guibg=#85D3F2 gui=bold
-      hi pounceacceptbest cterm=bold guifg=black guibg=#85D3F2 gui=bold
+      hi pounceaccept cterm=bold guifg=white guibg=#3650f2 gui=bold
+      hi pounceacceptbest cterm=bold guifg=white guibg=#3650f2 gui=bold
       hi pouncecursor cterm = bold guifg=black guibg=#cbc3e3 gui=bold
     ]]
   end
